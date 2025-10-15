2a1e656f99cd9ef86725f95b1fd15d4a9f507709
RequinSoft <requinsoft@gmail.com>
2025-10-15 06:49:13 -0600

    Cambio de posición del icono de login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,9 +19,22 @@
                 </div>
                 <div class="col-md-auto p-3">
                     <div class="row align-items-center g-3">
-                        <div class="col-auto">
-                        </div>
                         @livewire('fecha-actual')
+                        <div class="col-auto">
+                            <ul class="navbar-nav navbar-nav-icons ms-auto flex-row align-items-center">
+                                <li class="nav-item dropdown"><a class="nav-link pe-0 ps-2" id="navbarDropdownUser" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <div class="avatar avatar-xl">
+                                        <img class="rounded-circle" src="{{ asset('assets/img/icons/user1 h 256.png') }}" alt="" /> 
+                                    </div>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-caret dropdown-caret dropdown-menu-end py-0" aria-labelledby="navbarDropdownUser">
+                                    <div class="bg-white dark__bg-1000 rounded-2 py-2">   
+                                        <a class="dropdown-item" href="{{ route('login') }}">Ingresar</a>
+                                    </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -26,19 +26,5 @@
 
                     </li>
                     </ul>
-                    <ul class="navbar-nav navbar-nav-icons ms-auto flex-row align-items-center">
-
-                    <li class="nav-item dropdown"><a class="nav-link pe-0 ps-2" id="navbarDropdownUser" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <div class="avatar avatar-xl">
-                            <img class="rounded-circle" src="{{ asset('assets/img/icons/user1 h 256.png') }}" alt="" /> 
-                        </div>
-                        </a>
-                        <div class="dropdown-menu dropdown-caret dropdown-caret dropdown-menu-end py-0" aria-labelledby="navbarDropdownUser">
-                        <div class="bg-white dark__bg-1000 rounded-2 py-2">   
-                            <a class="dropdown-item" href="{{ route('login') }}">Ingresar</a>
-                        </div>
-                        </div>
-                    </li>
-                    </ul>
                 </nav>
                 <!-- Termina Menú dispositivos -->
