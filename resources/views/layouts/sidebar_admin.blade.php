80b347646907f6d0b1c800c64ff28987555970a6
rs <requinsoft@gmail.com>
2025-11-17 12:39:23 -0600

    Configuracion Molienda

--- resources/views/layouts/sidebar_admin.blade.php
+++ resources/views/layouts/sidebar_admin.blade.php
@@ -65,7 +65,7 @@
             </a>
             <li class="nav-item">
               <!-- parent pages--><a class="nav-link dropdown-indicator" href="#" role="button" data-bs-toggle="collapse" aria-expanded="true" aria-controls="dashboard">
-                <div class="d-flex align-items-center"><span class="nav-link-icon"><span class="fas fa-globe"></span></span><span class="nav-link-text ps-1">Dashboard</span>
+                <div class="d-flex align-items-center"><span class="nav-link-icon"><span class="fas fa-globe"></span></span><span class="nav-link-text ps-1">Molienda</span>
                 </div>
               </a>
               <ul class="nav collapse show" id="dashboard">
